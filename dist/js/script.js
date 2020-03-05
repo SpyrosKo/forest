abed6c92b6355b956571fd5b7296afe7d41815d8
SpyrosKo <spyros.konofaos1@gmail.com>
2020-03-05 16:02:38 -0500

    FEAT: Finishing up  mobile navgation. Making sure everything fits fine

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,30 +1,8 @@
-const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-links');
-    const navLinks = document.querySelectorAll('.nav-links li');
-    const header = document.querySelector('#header');
-    const body = document.querySelector('#body');
+let line = document.querySelectorAll('.line');
+let hamburger = document.querySelector('#hamburger');
 
-
-    burger.addEventListener('click',()=>{
-        nav.classList.toggle('nav-active');
-        burger.classList.toggle('toggle');
-        header.classList.toggle('header-fade');
-        body.classList.toggle('scroll-hidden');
-
-
-
-        navLinks.forEach((link, index)=>{
-            if(link.style.animation){
-                link.style.animation = '' ;   
-            } else {
-                link.style.animation = `navLinkFade 0.2s ease forwards ${index / 7}s`;
-            }
-        });
-
-       
-
-    });
+document.querySelector('#hamburger').onclick = function () {
+   for (let i = 0; line.length; i++) {
+      line[i].classList.toggle('toggle-color');
+   }
 };
-
-navSlide();

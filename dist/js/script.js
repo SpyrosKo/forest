a5a64713eb5e12a148a7d35ce32c2923788baf47
SpyrosKo <spyros.konofaos1@gmail.com>
2020-03-05 21:11:38 -0500

    Feat: Completing website. Social Links images  not working on live site

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,8 +1,26 @@
 let line = document.querySelectorAll('.line');
 let hamburger = document.querySelector('#hamburger');
+let link = document.querySelectorAll('.navigation__link');
+
+// document.querySelector('#hamburger').onclick = function () {
+//    for (let i = 0; line.length; i++) {
+//       line[i].classList.toggle('toggle-color');
+//    }
+// };
+
+// document.querySelector('#hamburger').onclick = function () {
+//    for (let i = 0; line.length; i++) {
+//       line[i].classList.toggle('medium-toggle-color');
+//    }
+// };
+
 
 document.querySelector('#hamburger').onclick = function () {
-   for (let i = 0; line.length; i++) {
+   for (let i = 0; i < link.length; i++) {
+      link[i].classList.toggle('toggle-link-color');
+
+      line[i].classList.toggle('medium-toggle-color');
+
       line[i].classList.toggle('toggle-color');
    }
 };

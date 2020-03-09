c8530d3f00d59f89aa78b21ef0575dbe0345d0b4
SpyrosKo <spyros.konofaos1@gmail.com>
2020-03-09 11:14:53 -0400

    Updates

--- js/script.js
+++ js/script.js
@@ -1,21 +1,21 @@
-let line = document.querySelectorAll('.line');
-let hamburger = document.querySelector('#hamburger');
-let link = document.querySelectorAll('.navigation__link');
-let body = document.querySelector('#body');
+// let line = document.querySelectorAll('.line');
+// let hamburger = document.querySelector('#hamburger');
+// let link = document.querySelectorAll('.navigation__link');
+// let body = document.querySelector('#body');
 
-document.querySelector('#hamburger').onclick = function () {
-   for (let i = 0; i < link.length; i++) {
-      link[i].classList.toggle('toggle-link-color');
+// document.querySelector('#hamburger').onclick = function () {
+//    for (let i = 0; i < link.length; i++) {
+//       link[i].classList.toggle('toggle-link-color');
 
-      line[i].classList.toggle('medium-toggle-color');
-   }
-};
+//       line[i].classList.toggle('medium-toggle-color');
+//    }
+// };
 
-document.querySelector('#body').onclick = function () {
+// document.querySelector('#body').onclick = function () {
 
-   for (let i = 0; i < line.length; i++) {
-      // line[i].classList.toggle('medium-toggle-color');
+//    for (let i = 0; i < line.length; i++) {
+//       // line[i].classList.toggle('medium-toggle-color');
 
-      // line[i].classList.toggle('toggle-color');
-   }
-};
+//       // line[i].classList.toggle('toggle-color');
+//    }
+// };

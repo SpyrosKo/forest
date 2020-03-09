2f0f28bd01c233b4e7f85883640863898ac04e3f
SpyrosKo <spyros.konofaos1@gmail.com>
2020-03-09 10:46:37 -0400

    Feat: current

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,26 +1,21 @@
 let line = document.querySelectorAll('.line');
 let hamburger = document.querySelector('#hamburger');
 let link = document.querySelectorAll('.navigation__link');
-
-// document.querySelector('#hamburger').onclick = function () {
-//    for (let i = 0; line.length; i++) {
-//       line[i].classList.toggle('toggle-color');
-//    }
-// };
-
-// document.querySelector('#hamburger').onclick = function () {
-//    for (let i = 0; line.length; i++) {
-//       line[i].classList.toggle('medium-toggle-color');
-//    }
-// };
-
+let body = document.querySelector('#body');
 
 document.querySelector('#hamburger').onclick = function () {
    for (let i = 0; i < link.length; i++) {
       link[i].classList.toggle('toggle-link-color');
 
       line[i].classList.toggle('medium-toggle-color');
+   }
+};
 
-      line[i].classList.toggle('toggle-color');
+document.querySelector('#body').onclick = function () {
+
+   for (let i = 0; i < line.length; i++) {
+      // line[i].classList.toggle('medium-toggle-color');
+
+      // line[i].classList.toggle('toggle-color');
    }
 };
